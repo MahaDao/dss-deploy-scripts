094d2eafe854db56aba0464bb92abe1731b84151
Steven Enamakel <enamakel@cryptocontrol.io>
2020-06-02 23:55:44 +0530

    update src.nix

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/maharajadao/makerpkgs";
-    rev = "a02a448bc4da740d84f80faa9dcbd98b211c7d8c";
+    rev = "6b8d4babdf9927ce1373c07b6bf93305de3aeb61";
     ref = "mcd-fix";
   };
 }
