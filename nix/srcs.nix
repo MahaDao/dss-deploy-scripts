55ae44cd5efb43d3c6b6b7c7779e92e0406f9d31
Steven Enamakel <enamakel@cryptocontrol.io>
2020-06-02 18:11:52 +0530

    fix mcd

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -3,7 +3,6 @@ rec {
   makerpkgs = fetchGit {
     url = "https://github.com/maharajadao/makerpkgs";
     rev = "a02a448bc4da740d84f80faa9dcbd98b211c7d8c";
-    ref = "master";
+    ref = "mcd-fix";
   };
 }
-
